228de568293a3f9ca3175177b7b3a5a64f851b59
RCPilot1604 <ethan.chua1604@gmail.com>
2025-07-31 18:00:32 +0800

    Modify debug statement for greater clairty

--- flask/midi_parser.py
+++ flask/midi_parser.py
@@ -300,14 +300,14 @@ class MidiParser:
         """Export parsed MIDI data to JSON file"""
         try:
             # Convert each event to a dict
-            serializable_events = [[e.to_dict() for e in note_events] for note_events in events]
+            serializable_events = [[{"note" : i, "data" : e.to_dict()} for i, e in note_events] for note_events in events]
             with open('./tmp/midi_sanitized_unconverted.json', 'w') as f:
                 json.dump(serializable_events, f, indent=2)
             return True
         except Exception as e:
             print(f"Error exporting to JSON: {e}")
             return False
-        
+            
     def _export_to_json_flatlist(self, events: list) -> bool:
         """Export parsed MIDI data to JSON file"""
         try:
