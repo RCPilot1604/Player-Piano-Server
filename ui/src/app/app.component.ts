30835f2b6466a0dae9b53b8950c8481f6ee81a5e
RCPilot1604 <ethan.chua1604@gmail.com>
2025-07-31 16:41:38 +0800

    Update app component to include visualizer for midi falling tiles

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -9,16 +9,22 @@ import { AddSongDialogComponent } from './add-song-dialog/add-song-dialog.compon
 import { MatDialog } from '@angular/material/dialog';
 import { AddCategoryDialogComponent } from './add-category-dialog/add-category-dialog/add-category-dialog.component';
 import { WebsocketService } from './services/websocket.service';
+import { MidiFallingTilesComponent } from './visualizer/visualizer.component';
+import { MidiEvent } from './models/midi-event.model';
+import { TileEvent } from './models/tile-event.model';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SongListComponent, MatToolbarRow, MatIconModule, MatButtonModule, MatMenuModule],
+  imports: [RouterOutlet, SongListComponent, MatToolbarRow, MatIconModule, MatButtonModule, MatMenuModule, MidiFallingTilesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ui';
   currentTime = 0;
-  constructor(public dialog: MatDialog, private socket: WebsocketService) {}
+  midiData: MidiEvent[] = [];
+  tileData: TileEvent[] = [];
+  constructor(public dialog: MatDialog, private socket: WebsocketService) { }
 
   openAddSongDialog(): void {
     const dialogRef = this.dialog.open(AddSongDialogComponent, {
@@ -34,5 +40,27 @@ export class AppComponent {
     this.socket.fromEvent('timeUpdate').subscribe((data) => {
       this.currentTime = data;
     });
+    this.socket.fromEvent('midiData').subscribe((data) => {
+      console.log('Received MIDI data:', data);
+      this.midiData = data;
+    });
+    this.socket.fromEvent('tileUpdate').subscribe(() => {
+      console.log('Received tile update');
+      fetch('httpApi/tiles')
+        .then(response => {
+          if (response.status === 500) {
+            console.error('Server error: 500');
+            this.tileData = [];
+            return;
+          }
+          return response.json();
+        })
+        .then(json => {
+          if (json) {
+            this.tileData = json;
+          }
+        });
+    });
+
   }
 }
