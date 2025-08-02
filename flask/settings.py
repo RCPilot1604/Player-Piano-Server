1a7e21dd0b2762b7b686f866dcaed727726bcb85
RCPilot1604 <ethan.chua1604@gmail.com>
2025-08-02 16:02:40 +0800

    Further debug

--- flask/settings.py
+++ flask/settings.py
@@ -5,7 +5,7 @@ class Settings:
     
     def __init__(self):
         self.settings = {
-            'midi_json_file_path': './tmp/midi_data.json',
+            'midi_json_file_path': './tmp/final_data.json',
             'midi_tile_data_file_path': './tmp/midi_tile_data.json',
             'clock_period': 0.02, # 20 ms
             'lowest_note': 21,  # A0
