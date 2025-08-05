67183d111193c30d39ac980b78eb402f21530be5
RCPilot1604 <ethan.chua1604@gmail.com>
2025-08-05 15:47:53 +0800

    Add note-color module

---
+++ ui/src/app/models/note-colors.model.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_NOTE_COLORS: string[] = [
+  "#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#A133FF",
+  "#33FFF6", "#FF8633", "#33FF86", "#8633FF", "#FF3386",
+  "#86FF33", "#3386FF", "#FFB533", "#B5FF33", "#33B5FF",
+  "#FF33B5", "#B533FF", "#33FFB5", "#FF6F33", "#6FFF33"
+];
