3996a32b01bf2ee0634fb3635decf6d581bbc5c3
RCPilot1604 <ethan.chua1604@gmail.com>
2025-08-31 17:27:18 +0800

    Modify filepaths

--- flask/settings.py
+++ flask/settings.py
@@ -5,8 +5,8 @@ class Settings:
     
     def __init__(self):
         self.settings = {
-            'midi_json_file_path': '/tmp/final_data.json',
-            'midi_tile_data_file_path': './tmp/midi_tile_data.json',
+            'midi_json_file_path': './tmp/final_data.json',
+            'midi_tile_data_file_path': 'tmp/midi_tile_data.json',
             'clock_period': 0.02, # 20 ms
             'lowest_note': 21,  # A0
             'highest_note': 108,  # C8
